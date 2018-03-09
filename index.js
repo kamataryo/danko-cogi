00ef972a6b2f324601bdb428d57c2fb064d89367
kamataryo <mugil.cephalus+ciconia.local@gmail.com>
2018-03-09 19:17:06 +0900

    Fix some

--- index.js
+++ index.js
@@ -3,36 +3,44 @@ const ejs = require('ejs')
 const { promisify } = require('util')
 const readFile = promisify(require('fs').readFile)
 const path = require('path')
+const url = require('url')
+
+const {
+  COGI_FACEBOOK_APP_ID,
+  COGI_TWITTER_ID,
+  COGI_HOME_URL,
+  COGI_PORT,
+} = process.env
 
 fastify.register(require('fastify-static'), {
   root: path.join(__dirname, 'assets'),
   prefix: '/assets/'
 })
 
-const { COGI_FACEBOOK_APP_ID, COGI_TWITTER_ID, COGI_HOME_URL, COGI_PORT } = process.env
-
 readFile('./templates/protest.html.ejs').then(file => {
   const protest = file.toString()
 
+  const render = (req, rep) => {
+    const content = url.parse(req.params['*']).pathname.split('/')[1]
+
+    if (content.length > 255) {
+      rep.header('Content-Type', 'text/plane').code(400).send('文字長すぎ')
+    }
+
+    rep.header('Content-Type', 'text/html').send(
+      ejs.render(protest, {
+        content,
+        COGI_FACEBOOK_APP_ID,
+        COGI_TWITTER_ID,
+        COGI_HOME_URL
+      })
+    )
+  }
+
   fastify
-    .get('/', (req, rep) => {
-      const { content } = req.query
-      rep.header('Content-Type', 'text/html').send(
-        ejs.render(protest, {
-          content,
-          COGI_FACEBOOK_APP_ID,
-          COGI_TWITTER_ID,
-          COGI_HOME_URL
-        })
-      )
-    })
-    .get('/cogi.svg', (req, rep) => {
-      rep.sendFile('cogi.svg')
-    })
-    .get('/cogi.png', (req, rep) => {
-      console.log('aaa')
-      rep.sendFile('cogi.png')
-    })
+    .get('/cogi.svg', (req, rep) => rep.sendFile('cogi.svg'))
+    .get('/cogi.png', (req, rep) => rep.sendFile('cogi.png'))
+    .get('*', render)
 
   fastify.listen(COGI_PORT || 3000, '127.0.0.1', err => {
     if (err) {
