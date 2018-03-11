723ad69c8f8377c296f1bb13782ad8a3556666dd
kamataryo <mugil.cephalus+ciconia.local@gmail.com>
2018-03-11 15:01:02 +0900

    Fix twitter link encoding

--- assets/script.js
+++ assets/script.js
@@ -4,9 +4,9 @@ const tweet = document.getElementById('tweet')
 input.addEventListener('change', e => {
   const title = encodeURIComponent('断固コーギー')
   const url = `${window.location.href.split('?')[0]}words/${encodeURIComponent(
-    e.target.value || ''
+    encodeURIComponent(e.target.value || '')
   )}`
-
+  console.log(url)
   tweet.setAttribute(
     'href',
     `https://twitter.com/intent/tweet?text=${title}&url=${url}`
