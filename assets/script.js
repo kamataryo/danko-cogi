301e103e218ad53ec6e77491f68f54b41cab772a
kamataryo <mugil.cephalus+ciconia.local@gmail.com>
2018-03-11 21:45:00 +0900

    Fix consuct and style

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,7 @@ const tweet = document.getElementById('tweet')
 input.addEventListener('change', e => {
   const text = encodeURIComponent('断固コーギー')
 
+  // this code run only with route '/'.
   const url = `${window.location.href.split('?')[0]}words/${encodeURIComponent(
     encodeURIComponent(e.target.value || '')
   )}`
